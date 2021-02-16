29c2d5a2f2f8687f92b4e753fea4faeb974786d1
laminegaci <mohamed61lamine@gmail.com>
2021-02-16 16:30:30 +0100

    finishing design and implementing map

---
+++ app/js/script.js
@@ -0,0 +1,30 @@
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoibW9oYW1lZGxhbWluZSIsImEiOiJja2w4M3l2NnEwOHJkMm9vYnVxaWUyc2NwIn0.9LsTpB92oJReTGwGyKAQFA'
+}).addTo(mymap);
+
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
